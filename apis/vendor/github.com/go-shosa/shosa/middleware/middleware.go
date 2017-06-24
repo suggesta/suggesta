0dcc00bd180d51ca6cf10c4639e1fdca9668d2fb
Yusuke Komatsu <tienlen042@gmail.com>
2017-06-25 03:08:02 +0900

    remove vendor

--- apis/vendor/github.com/go-shosa/shosa/middleware/middleware.go
+++
@@ -1,8 +0,0 @@
-package middleware
-
-import "github.com/labstack/echo"
-
-// defaultSkipper returns false which processes the middleware.
-func defaultSkipper(c echo.Context) bool {
-	return false
-}
--- apis/vendor/github.com/kyokomi/slackbot/cmd/slackbotcmd/version.go
+++
@@ -1,4 +0,0 @@
-package main
-
-const Name string = "slackbotcmd"
-const Version string = "0.2.0"
