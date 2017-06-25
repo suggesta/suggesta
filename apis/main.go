d3c5041518d47d8064814248f17a1a97e35af5ae
Yusuke Komatsu <tienlen042@gmail.com>
2017-06-25 12:27:28 +0900

    fix add CORS middleware

--- apis/main.go
+++ apis/main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
+	myMW "github.com/go-shosa/shosa/middleware"
 	"github.com/go-shosa/shosa/server"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
@@ -26,5 +30,16 @@ func main() {
 	log.Printf("start pid %d", os.Getpid())
 
 	// Server runs
-	server.Run(url, Routes)
+	server.RunWithConfig(server.Server{
+		Middlewares: []echo.MiddlewareFunc{
+			middleware.Recover(),
+			middleware.CORS(),
+			myMW.RequestID(),
+			myMW.Logger(),
+		},
+		ShutdownTimeout: 15 * time.Second,
+		URI:             url,
+		Routing:         Routes,
+	})
+	// server.Run(url, Routes)
 }
